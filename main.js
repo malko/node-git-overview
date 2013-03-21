808a79449ee6dbd703938c4aa38980b7ec0488da
Jonathan Gotti <jgotti@elma.fr>
2013-03-21 17:54:42 +0100

    adding a static webserver + basic json views

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var child_process = require('child_process')
 	, fs = require('fs')
+    , http = require('http')
 	, D = require('./D.js')
     , DEBUG_MODE = false
     , log = function(){
@@ -37,7 +38,7 @@ function updateConfig(){
 */
 function parseBranches(str,repoConfig){
     var res=[],exp=new RegExp("(?:Merge branch(?:es)?|,|and)\\s+?'([^']+)'\\s*(?=,|and |into "+repoConfig.testing+")",'g');
-    str.replace(exp, function(m,branch){ res.push(branch);});
+    str.replace(exp, function(m,branch){ res.push({name:branch});});
     log('\nPARSEBRANCHES:\n',str,exp,res,'\n');
     return res;
 }
@@ -78,7 +79,7 @@ function updateRepoStatus(repoName,remoteUpdateFirst){
                 });
             });
             repoStatuses[repoName].merges.sort(function(a,b){ return a.at > b.at ? -1 : (a.at===b.at?0:1);});
-            //return repoStatuses
+            return repoStatuses[repoName];
         })
         .rethrow(function(err){
             repoStatuses[repoName] = {error:err.toString()};
@@ -99,6 +100,77 @@ function updateRepoStatuses(remoteUpdateFirst){
     return D.all(promises);
 }
 
+// starting the server
+updateConfig()
+    .then(function(){ updateRepoStatuses(true) })
+    .then(function(){
+        http.createServer(function (req, res) {
+          //res.writeHead(200, {'Content-Type': 'application/json'});
+
+          switch(req.url){
+            case '/getStatuses':
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(repoStatuses));
+                break;
+            case 'D.js':
+                readFile('./D.js')
+                    .success(function(data){
+                        res.writeHead(500, {"Content-Type":'application/javascript'});
+                        res.end(data, "binary");
+                    })
+                    .error(function(err){
+                        res.writeHead(500, {"Content-Type": "text/plain"});
+                        res.end(err + "\n");
+                    })
+                ;
+            default:
+                //first lookup for a matching static file
+                if( req.url.match(/\/(|index(?:\.html)?|(?:basic-compat|stpl)\.js)$/) ){
+                    if( req.url === '/' || req.url === '/index' ){
+                        req.url = '/index.html';
+                    }
+                    readFile('public'+req.url)
+                        .success(function(data){
+                            req.url.replace(/\.(js|css|html|htm)$/,function(m,ext){
+                                var ctype='text/html';
+                                (ext==='css') && (ctype='text/css');
+                                (ext==='js') && (ctype='application/javascript');
+                                res.setHeader("Content-Type",ctype);
+                            });
+                            res.writeHead(200);
+                            res.write(data, "binary");
+                            res.end();
+                        })
+                        .error(function(err){
+                            res.writeHead(500, {"Content-Type": "text/plain"});
+                            res.write(err + "\n");
+                            res.end();
+                        })
+                    ;
+                    break;
+                }
+                //then trying to match dynamic pathes
+                var params =  req.url.match(/^\/getStatus\/([^\/]+)$/);
+                if( null===params ){
+                    console.log('404',req.url);
+                    res.writeHead(404, {'Content-Type': 'text/html'});
+                    res.end('<h1>404</h1> Page unknown');
+                    break;
+                }
+                console.log(req.url,params);
+                updateRepoStatus(params[1],true)
+                    .then(function(status){
+                        res.writeHead(200, {'Content-Type': 'application/json'});
+                        res.end(JSON.stringify(status));
+                    })
+                ;
+                break;
+          }
+        }).listen(8080, '127.0.0.1');
+        console.log('running');
+    })
+;
+return ;
 updateConfig()
     .then(updateRepoStatuses)
 	.then(function(){
